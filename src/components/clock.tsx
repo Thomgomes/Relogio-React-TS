0b91c5d9caea74401b91840fbc43617d57839693
ThomGomes <math.011@hotmail.com>
2022-11-21 09:39:25 -0300

    pointer ok

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 import './clock.css'
+
 import { HourPointer } from './hours';
 import { MinutesPointer } from './minutes';
 import { SecondsPointer } from './seconds';
 
+setInterval(() => {
+  const deg = 6;
+  const hr = document.getElementById('hr');
+  const mn = document.getElementById('mn');
+  const sec = document.getElementById('sec');
+  
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+  
+    hr!.style.transform = `rotateZ(${(hh)+(mm/12)}deg)` 
+    mn!.style.transform = `rotateZ(${mm}deg)`
+    sec!.style.transform = `rotateZ(${ss}deg)`
+  })
+
+  
+
 export const Clock = () => {
   return (
     <>
-      <div className="clock w-72 h-72 flex justify-center items-center bg-cover z-2
+      <div className="clock mt-20 w-72 h-72 flex justify-center items-center bg-cover z-2 bg-slate-50
       border-4 border-solid border-neutral-800 rounded-full
-      before:content-[''] before:absolute before:h-2 before:w-2 before:bg-white 
+      before:content-[''] before:absolute before:h-2 before:w-2 before:bg-yellow-700
       before:rounded-full before:z-6">
         <HourPointer />
         <MinutesPointer />
@@ -19,20 +38,3 @@ export const Clock = () => {
     </>
   );
 }
-
-
-const deg = 6;
-const hr = document.querySelector<HTMLElement>('.hr')!;
-const mn = document.querySelector<HTMLElement>('.mn')!;
-const sec = document.querySelector<HTMLElement>('.sec')!;
-
-setInterval(() => {
-  let day = new Date();
-  let hh = day.getHours() * 30;
-  let mm = day.getMinutes() * deg;
-  let ss = day.getSeconds() * deg;
-
-  hr["style"].transform = `rotateZ(${(hh)+(mm/12)}deg)` 
-  mn["style"].transform = `rotateZ(${mm}deg)`
-  sec["style"].transform = `rotateZ(${ss}deg)`
-})
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import './header.css'
 export const Header = () => {
   return (
     <>
-      <header className="fixed w-full h-auto flex flex-col justify-center items-center 
+      <header className="fixed w-full min-w-min h-auto flex flex-col justify-center items-center 
       place-items-stretch p-9 -mt-3 text-green-300">
         <h1 className=" text-6xl font-bold mb-2 mt-5" >Relógio</h1>
       <Subtitle/>
